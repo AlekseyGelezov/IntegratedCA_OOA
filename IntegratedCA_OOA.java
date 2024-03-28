ef98b22e1902aa2aa44132d7af4615915490f585
AlekseyGelezov <152330869+AlekseyGelezov@users.noreply.github.com>
2024-03-28 16:09:28 +0000

    BUG FIXES & UPDATES TO REPORT GENERATION
    Multiple errors were fixed, report generation & terminal was updated.
    
    Currently only Course Report is generated. Will add the rest later.
    
    Main method is used to call upon the other two methods.

--- IntegratedCA_OOA.java
+++ IntegratedCA_OOA.java
@@ -19,25 +19,23 @@ public class IntegratedCA_OOA {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
-    }
+    
     
     
     // ALLOWS ACCESS OF MYSQL DATABASE & TABLES. ALSO DEFINES STRINGS AND SCANNER REQUIRED FOR TERMINAL USAGE
     private static String SQLurl = "jdbc:mysql://localhost:3306/col_database";
     private static String SQLuser = "aleksey";
     private static String SQLpassword = "26325againA";
-    public static Scanner admininput = new Scanner(System.in);
+    private static Scanner user_input = new Scanner(System.in);
     private static String terminaluser;
 
     
     // USER TERMINAL, ALLOWS USER TO LOG IN AS ADMIN, OFFICE OR LECTURER
 public static void collegeLoginInput(){
         System.out.println("Please enter your username: ");
-        String col_username = admininput.nextLine();
+        String col_username = user_input.nextLine();
         System.out.println("Please enter your password: ");
-        String col_password = admininput.nextLine();
+        String col_password = user_input.nextLine();
        
         if(col_username.equals("admin") && col_password.equals("java")){
             terminaluser = "admin";
@@ -52,30 +50,93 @@ public static void collegeLoginInput(){
             System.out.println("Login failed. Check if your username or password was correct.");
             collegeLoginInput();
         }
-      }
+
 }
 
-//        
-//        // PRINTS OUT A COURSE REPORT
-//        String coursereportquery = "SELECT * FROM courses";
-//        try (Connection connection = DriverManager.getConnection(url, user, password);
-//             Statement statement = connection.createStatement();
-//             ResultSet resultSet = statement.executeQuery(coursereportquery)) {
-//
-//            // Process the results
-//            while (resultSet.next()) {
-//                String programmeid = resultSet.getString("programme_id");
-//                String programmename = resultSet.getString("programme_name");
-//                String programmecapacity = resultSet.getString("programme_capacity");
-//                String moduleid = resultSet.getString("module_id");
-//                String modulename = resultSet.getString("module_name");
-//                String modulecapacity = resultSet.getString("module_capacity");
-//                String location = resultSet.getString("location");
-//                String lecturerid = resultSet.getString("lecturer_id");
-//                        
-//                System.out.println("Programme ID: " + programmeid + ",Programme Name: " + programmename + ",Programme Capacity: " + programmecapacity + ",Module ID: " + moduleid + ",Module Name: " + modulename +",Module Capacity: " + modulecapacity + ",Located: " + location + ",Lecturer ID: " + lecturerid);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-    
+
+public static void displayConsole(){
+        try {
+        FileWriter c_report = new FileWriter("c_report.csv");
+        
+        FileWriter s_report = new FileWriter("s_report.csv");
+        
+        FileWriter l_report = new FileWriter("l_report.csv");
+       
+        Connection connection = DriverManager.getConnection(SQLurl, SQLuser, SQLpassword);
+        System.out.println("College Database Connection Established!");
+       
+        String CourseReportQuery = "SELECT * FROM Courses";
+        PreparedStatement c_Statement = connection.prepareStatement(CourseReportQuery);
+        ResultSet c_result = c_Statement.executeQuery();
+       
+        String StudentReportQuery = "SELECT * FROM Students";
+        PreparedStatement s_Statement = connection.prepareStatement(StudentReportQuery);
+        ResultSet s_result = s_Statement.executeQuery();
+       
+        String LecturerReportQuery = "SELECT * FROM Lecturers";
+        PreparedStatement l_Statement = connection.prepareStatement(LecturerReportQuery);
+        ResultSet l_result = l_Statement.executeQuery();
+       
+        while (true){
+            System.out.println("Enter \"1 to generate a Course Report\", \"2 to generate a Student Report\", \"3 to generate a Lecturer Report\" , \"4 to change password\", \"5 to Log out\"");
+            if(terminaluser.equals("admin")){
+                System.out.println("\"6 to Manage Users\"");
+            }
+            System.out.println("Select an option: ");
+            int user_choice = user_input.nextInt();
+            user_input.nextLine();
+       
+        switch (user_choice) {
+        case 1:
+// PRINTS OUT A COURSE REPORT
+            while (c_result.next()) {
+                String programmeid = c_result.getString("programme_id");
+                
+                String programmename = c_result.getString("programme_name");
+                
+                String programmecapacity = c_result.getString("programme_capacity");
+                
+                String moduleid = c_result.getString("module_id");
+                
+                String modulename = c_result.getString("module_name");
+                
+                int modulecapacity = c_result.getInt("module_capacity");
+                
+                String location = c_result.getString("location");
+                
+                String lecturerid = c_result.getString("lecturer_id");
+                       
+                
+                c_report.append((programmeid)).append(",");
+                
+                c_report.append((programmename)).append(",");
+                
+                c_report.append((programmecapacity)).append(",");
+                
+                c_report.append((moduleid)).append(",");
+                
+                c_report.append((modulename)).append(",");
+                
+                c_report.append(String.valueOf(modulecapacity)).append(",");
+                
+                c_report.append((location)).append(",");
+                
+                c_report.append((lecturerid)).append(",");
+            }
+            
+        //write here outside while
+            c_report.flush();
+            c_report.close();
+            break;
+        }
+        }
+        }catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+        
+}
+public static void main(String[] args) {
+        collegeLoginInput();
+        displayConsole();
+}
+}
